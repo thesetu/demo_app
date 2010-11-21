250da37c1ca110b86ab284e08902c87319514982
Zahiduzzaman <thesetu@gmail.com>
2010-11-21 22:38:22 +0600

    First Commit To the Demo APP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b10682863bc7b71a75c7f1e7b78e971d78c1706cb359f7ca761e0524f95e2df5dc053b585d38c002d53853b1e8ef1bb9d250aa178c0f73dd873aaebdc03ceba6'
---
+++ init.cmd
@@ -0,0 +1,7 @@
+git config --global user.name "Zahiduzzaman"
+
+git config --global user.email thesetu@gmail.com
+
+git init
+
+komodo .gitignore
